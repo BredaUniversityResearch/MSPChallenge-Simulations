7f59de622af132b499eb95d214b1eba18b342b8f
Marin <hekman.m@buas.nl>
2023-01-19 19:30:09 +0100

    improved build.bat to support calls like: build.bat "output_path=..\MSPChallenge-Server\simulations\" "publish_targets[0]=alpine.3.17-x64" "configuration=Debug"

--- build.bat
+++ build.bat
@@ -1,18 +1,37 @@
-@echo off
-setlocal
+@(
+  setlocal
+  echo off
+  for %%_ in (%*) do set "%%~_"
+)
+
 set cwd=%cd%
-set api_version=v1
-set publish_targets[0]=win-x64
-set publish_targets[1]=alpine.3.17-x64
-rem set publish_targets[2]=ubuntu.22.04-x64
+if "%configuration%" == "" (
+    set configuration=Release
+)
+if "%api_version%" == "" (
+    set api_version=v1
+)
+if "%publish_targets[0]%" == "" (
+    set publish_targets[0]=alpine.3.17-x64
+    set publish_targets[1]=win-x64
+    rem set publish_targets[2]=ubuntu.22.04-x64
+)
+if "%output_path%" == "" (
+    set output_path=%cwd%\output
+)
+if "%output_path:~0,2%" == ".." (
+    set output_path=%cwd%\%output_path%
+)
+
+echo using output path: %output_path%
 
 call :build MSWSupport
-copy /y MSWSupport\MSWSupport\bin\Release\net6.0\MSWSupport.dll DLLs\
+copy /y MSWSupport\MSWSupport\bin\%configuration%\net6.0\MSWSupport.dll DLLs\
 
 call :build SELRELBridge
-copy /y SELRELBridge\SELRELBridge\bin\Release\net6.0\SELRELBridge.dll DLLs\
+copy /y SELRELBridge\SELRELBridge\bin\%configuration%\net6.0\SELRELBridge.dll DLLs\
 
-rmdir /q /s output > nul 2> nul
+rmdir /q /s "%output_path%" > nul 2> nul
 
 call :publish CEL %api_version%\
 call :publish MEL %api_version%\
@@ -38,7 +57,7 @@ if not exist "%1" (
     goto eof
 )
 cd "%1"
-dotnet build -c Release
+dotnet build -c %configuration%
 cd "%cwd%"
 exit /b 0
 
@@ -62,11 +81,15 @@ exit /b 0
 if not defined publish_targets[%x%] exit /b 0
 call set target=%%publish_targets[%x%]%%
 echo Publishing %target%...
-dotnet publish -c Release -r %target% --self-contained
-set target_dir=%cwd%\output\%target%\%1\%2
-echo mkdir %target_dir% > nul 2> nul
+dotnet publish -c %configuration% -r %target% --self-contained
+
+if "%publish_targets[1]%" == "" (
+    set target_dir=%output_path%\%api_version%\
+) else (
+    set target_dir=%output_path%\%target%\%api_version%\
+)
+
 mkdir %target_dir% > nul 2> nul
-echo copy %1\bin\Release\net6.0\%target%\publish\* %target_dir%
-copy /y %1\bin\Release\net6.0\%target%\publish\* %target_dir%
+copy /y %1\bin\%configuration%\net6.0\%target%\publish\* %target_dir%
 SET /a "x+=1"
 goto :publish_targets_loop
